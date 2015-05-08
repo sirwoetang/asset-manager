4bdf8677fce8d52dcb31ff6c96f906b48ccf9731
Waldo Boshoff (Mac) <wakkerakker@gmail.com>
2015-05-08 17:40:47 +0200

    adding init.js to all files.

---
+++ www/assets/js/init.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Waldo on 08/05/2015.
+ */
+function onDeviceReady() {
+    var tempStorage = window.sessionStorage;
+}
--- www/assets/js/scanner.js
+++ www/assets/js/scanner.js
@@ -8,10 +8,12 @@ function startScan() {
             var s = "Result: " + result.text + "<br/>" +
                 "Format: " + result.format + "<br/>" +
                 "Cancelled: " + result.cancelled;
-            resultDiv.innerHTML = s;
+            //resultDiv.innerHTML = s;
+            return s
         },
         function (error) {
             alert("Scanning failed: " + error);
+            var s = "error"
         }
     );
 }
