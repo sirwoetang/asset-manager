4e02c332d8b1b64bb82a1d7b818df3f3f9ad73e8
Waldo Boshoff (Mac) <wakkerakker@gmail.com>
2015-05-08 17:31:53 +0200

    page tree completed

--- www/assets/js/scanner.js
+++ www/assets/js/scanner.js
@@ -2,14 +2,16 @@
  * Created by Waldo on 08/05/2015.
  */
 
-cordova.plugins.barcodeScanner.scan(
-    function (result) {
-        alert("We got a barcode\n" +
-        "Result: " + result.text + "\n" +
-        "Format: " + result.format + "\n" +
-        "Cancelled: " + result.cancelled);
-    },
-    function (error) {
-        alert("Scanning failed: " + error);
-    }
-);
+function startScan() {
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            var s = "Result: " + result.text + "<br/>" +
+                "Format: " + result.format + "<br/>" +
+                "Cancelled: " + result.cancelled;
+            resultDiv.innerHTML = s;
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+}
